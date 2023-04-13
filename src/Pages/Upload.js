90e68d51e0d6b99a166ff57fa4f041ad24b547b0
aborgono <albertoiborgonovo@gmail.com>
2023-04-13 13:50:47 -0400

    completed bulk of upload form

--- src/Pages/Upload.js
+++ src/Pages/Upload.js
@@ -1,11 +1,33 @@
 import './Upload.scss';
-import Header from '../components/Header/Header';
+// import { Link } from 'react-router-dom';
 
 function Upload() {
     return (
-        <div>
-            <Header />
-        </div>
+        <section className='upload__container'>
+            <div className='upload__header__box'>
+                <h1 className='upload__header'> Upload Video</h1>
+            </div>
+            <div className='upload__main__content__box'>
+                <div className='upload__main__content__video'>
+                    <h3>VIDEO THUMBNAIL</h3>
+                </div>
+                <div className='upload__main__content__form'>
+                    <form id="comment__form-input" className="comment__form-input" novalidate>
+                        <div className="comment__pic"></div>
+                        <div className="comment__div">
+                            <label className="comment__name__type" for="fullName">TITLE YOUR VIDEO</label>
+                            <input className="comment__name__text" type="text" name="fullName" placeholder="Add a title to your video" required />
+                            <label className="comment__comment" for="comments">ADD A VIDEO DESCRIPTION</label>
+                            <textarea className="comment__comment__text" name="comments" id="comments" placeholder="Add a description to your video" required></textarea>
+                            <button className="button" type="submit">COMMENT</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div className='upload__submit__box'>
+
+            </div>
+        </section>
     );
 }
 
